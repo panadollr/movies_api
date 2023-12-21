e55a86f8e28953fdaf035d7bcfa2dfa5c94314fb
Apple M1 Max <cupleofgamer@gmail.com>
2023-12-21 11:40:40 +0700

    update code

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,7 +10,7 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'https://flashmov.vercel.app/');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-TOKEN');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
