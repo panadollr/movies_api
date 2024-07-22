c76f8c065c5ad47434794d2d129577e2d47bd33b
Nguyễn Văn Lân <90605381+panadollr@users.noreply.github.com>
2024-07-22 16:01:19 +0700

    Update MovieResource.php

--- app/Http/Resources/MovieResource.php
+++ app/Http/Resources/MovieResource.php
@@ -4,53 +4,19 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-use App\Models\Movie;
-
 class MovieResource extends JsonResource
 {
     protected $imageDomain;
-    protected $cloudinaryPosterDomain;
-    protected $cloudinaryThumbDomain;
     protected $imageController;
 
     public function __construct($resource)
     {
         parent::__construct($resource);
         $this->imageDomain = config('api_settings.image_domain');
-        if(request()->path() == 'xu-huong'){
-            $this->cloudinaryThumbDomain = "https://res.cloudinary.com/dtilp1gei/image/upload/c_thumb,w_1100/uploads/movies/";
-            $this->cloudinaryPosterDomain = "https://res.cloudinary.com/dtilp1gei/image/upload/ar_1:1,c_fill,g_auto/uploads/movies/";
-        } else {
-            $this->cloudinaryThumbDomain = "https://res.cloudinary.com/dtilp1gei/image/upload/c_thumb,w_280/uploads/movies/";
-            $this->cloudinaryPosterDomain = "https://res.cloudinary.com/dtilp1gei/image/upload/c_thumb,w_280/uploads/movies/";
-        }
     }
     
     public function toArray($request)
     {
-
-        // return array_filter([
-        //     'modified_time' => $this->modified_time,
-        //     'id' => $this->_id,
-        //     'name' => $this->name,
-        //     'origin_name' => $this->origin_name,
-        //     'poster_url' => $this->formatImageUrl($this->thumb_url),
-        //     'thumb_url' => $this->formatImageUrl($this->poster_url),
-        //     // 'poster_url' => $this->formatImageWithCloudinaryUrl('poster'),
-        //     // 'thumb_url' => $this->formatImageWithCloudinaryUrl('thumb'),
-        //     'slug' => $this->slug,
-        //     'year' => $this->year,
-        //     'content' => $this->content,
-        //     'type' => $this->type,
-        //     'status' => $this->status,
-        //     'sub_docquyen' => (bool) $this->sub_docquyen,
-        //     'time' => $this->time,
-        //     'episode_current' => $this->episode_current === 'Tập 0'? 'Trailer' : $this->episode_current,
-        //     'quality' => $this->quality,
-        //     'lang' => $this->lang,
-        //     'category' => $this->formattedCategoriesArray('category'),
-        //     // 'country' => $this->formattedArray('country'),
-        // ]);
 
         $categoryConfig = config('api_settings.categories');
         $countryConfig = config('api_settings.countries');
@@ -60,11 +26,10 @@ class MovieResource extends JsonResource
             'name' => $this->name,
             'poster_url' => $this->formatImageUrl($this->thumb_url),
             'thumb_url' => $this->formatImageUrl($this->poster_url),
-            // 'poster_url' => $this->formatImageWithCloudinaryUrl('poster'),
-            // 'thumb_url' => $this->formatImageWithCloudinaryUrl('thumb'),
             'slug' => $this->slug,
             'year' => $this->year,
             'content' => $this->content,
+            'trailer_url' => $this->trailer_url,
             'type' => $this->type,
             'status' => $this->status,
             'view' => $this->view,
@@ -74,19 +39,7 @@ class MovieResource extends JsonResource
             'quality' => $this->quality,
             'lang' => $this->lang,
             'category' => $this->formattedJsonWithConfig($this->category, $categoryConfig),
-            'country' => $this->formattedJsonWithConfig($this->country, $countryConfig),
-
-            // 'content' => $this->movie_detail->content,
-            // 'type' => $this->movie_detail->type,
-            // 'status' => $this->movie_detail->status,
-            // 'view' => $this->movie_detail->view,
-            // 'sub_docquyen' => (bool) $this->movie_detail->sub_docquyen,
-            // 'time' => $this->movie_detail->time,
-            // 'episode_current' => $this->movie_detail->episode_current === 'Tập 0'? 'Trailer' : $this->movie_detail->episode_current,
-            // 'quality' => $this->movie_detail->quality,
-            // 'lang' => $this->movie_detail->lang,
-            // 'category' => $this->formattedJsonWithConfig($this->movie_detail->category, $categoryConfig),
-            // 'country' => $this->formattedJsonWithConfig($this->movie_detail->country, $countryConfig),
+            // 'country' => $this->formattedJsonWithConfig($this->country, $countryConfig),
         ]);
     }
 
@@ -95,31 +48,6 @@ class MovieResource extends JsonResource
     {
         return "https://img.ophim.live/uploads/movies/{$this->slug}-thumb.jpg";
         
-    }
-
-    //poster cloudinary
-    protected function formatImageWithCloudinaryUrl($type)
-    {
-        $slug = $this->slug;
-        $cloudinaryFormat = "-$type.webp";
-        $ophimFormat = "-$type.jpg";
-            if ($type == 'thumb') {
-                if($this->year == 2023){
-                    $imageUrl = $this->cloudinaryThumbDomain . $slug . $cloudinaryFormat;
-                }else {
-                    $imageUrl = $this->imageDomain . $slug . $ophimFormat;
-                }
-            } else {
-                $modifiedTimeTimestamp = strtotime($this->modified_time);
-                $targetDateTimestamp = strtotime("10/1/2024");
-                if($modifiedTimeTimestamp > $targetDateTimestamp){
-                    $imageUrl = $this->imageDomain . $slug . $ophimFormat;
-                }else {
-                    $imageUrl = $this->cloudinaryPosterDomain . $slug . $cloudinaryFormat;
-                }
-            }
-        
-        return $imageUrl;
     }
 
     protected function formattedArray($propertyName)
@@ -131,22 +59,6 @@ class MovieResource extends JsonResource
             }, json_decode($propertyValue, true))
             : null;
     }
-
-    // protected function formattedCategoriesArray($propertyName)
-    // {
-    //     $propertyValue = $this->$propertyName;
-    //     $categories = config('api_settings.categories');
-    //     $propertyValue = collect(json_decode($this->$propertyName, true))->pluck('slug')->toArray();
-    
-    //     $filteredCategories = [];
-    //     foreach ($propertyValue as $categorySlug) {
-    //         if (array_key_exists($categorySlug, $categories)) {
-    //                 $filteredCategories[] = ['name' => $categories[$categorySlug]];
-    //         }
-    //     }
-    
-    //     return array_values($filteredCategories);
-    // }
 
     protected function formattedJsonWithConfig($jsonData, $arrayConfig)
 {
