99798680fb8c6eb10f0800e7c6b5c34c8cf09755
Nguyễn Văn Lân <90605381+panadollr@users.noreply.github.com>
2024-07-22 16:01:44 +0700

    Update Movie.php

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Movie extends Model
 {
@@ -21,6 +22,34 @@ class Movie extends Model
     //     'year',
     //     'poster_url'
     // ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Thêm global scope để chỉ lấy Manga có ít nhất một Chapter
+        static::addGlobalScope('countryScope', function (Builder $builder) {
+            $countries = ['Trung Quốc', 'Đài Loan', 'Hồng Kông'];
+
+            $builder->where(function($query) use ($countries) {
+                foreach ($countries as $country) {
+                    $query->orWhereRaw('JSON_CONTAINS(country, \'{"name": "' . $country . '"}\')');
+                }
+            });
+        });
+
+        // Thêm global scope để chỉ lấy Movie ngoại trừ một số danh mục
+        static::addGlobalScope('excludeCategories', function (Builder $builder) {
+            $excludedCategories = ['âm nhạc', 'tài liệu', '18+'];
+
+            $builder->where(function ($query) use ($excludedCategories) {
+                foreach ($excludedCategories as $category) {
+                    $query->whereRaw('NOT JSON_CONTAINS(category, \'{"name": "' . $category . '"}\')');
+                }
+            });
+        });
+    }
+
 
     /*
     |--------------------------------------------------------------------------
