b6cb05bf0a60d5c1ccee7abc589e797281e3d0fe
Nguyễn Văn Lân <90605381+panadollr@users.noreply.github.com>
2024-07-23 16:49:47 +0700

    Update MovieResource.php

--- app/Http/Resources/MovieResource.php
+++ app/Http/Resources/MovieResource.php
@@ -7,39 +7,38 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class MovieResource extends JsonResource
 {
     protected $imageDomain;
-    protected $imageController;
+    protected $type;
 
-    public function __construct($resource)
+    public function __construct($resource, $type = 'regular')
     {
         parent::__construct($resource);
         $this->imageDomain = config('api_settings.image_domain');
+        $this->type = $type;
     }
     
     public function toArray($request)
     {
 
         $categoryConfig = config('api_settings.categories');
-        $countryConfig = config('api_settings.countries');
-
+       
         return array_filter([
-            'id' => $this->_id,
-            'name' => $this->name,
-            'poster_url' => $this->formatImageUrl($this->thumb_url),
-            'thumb_url' => $this->formatImageUrl($this->poster_url),
-            'slug' => $this->slug,
-            'year' => $this->year,
-            'content' => $this->content,
-            'trailer_url' => $this->trailer_url,
-            'type' => $this->type,
-            'status' => $this->status,
-            'view' => $this->view,
-            'sub_docquyen' => (bool) $this->sub_docquyen,
-            'time' => $this->time,
-            'episode_current' => $this->episode_current === 'Tập 0'? 'Trailer' : $this->episode_current,
-            'quality' => $this->quality,
-            'lang' => $this->lang,
-            'category' => $this->formattedJsonWithConfig($this->category, $categoryConfig),
-            // 'country' => $this->formattedJsonWithConfig($this->country, $countryConfig),
+                'id' => $this->_id,
+                'name' => $this->name,
+                'poster_url' => $this->formatImageUrl($this->thumb_url),
+                'thumb_url' => $this->formatImageUrl($this->poster_url),
+                'slug' => $this->slug,
+                'year' => $this->year,
+                'content' => $this->content,
+                'trailer_url' => $this->trailer_url,
+                'type' => $this->type,
+                'status' => $this->status,
+                'view' => $this->view,
+                'sub_docquyen' => (bool) $this->sub_docquyen,
+                'time' => $this->time,
+                'episode_current' => $this->episode_current === 'Tập 0'? 'Trailer' : $this->episode_current,
+                'quality' => $this->quality,
+                'lang' => $this->lang,
+                'category' => $this->formattedJsonWithConfig($this->category, $categoryConfig),
         ]);
     }
 
@@ -50,26 +49,16 @@ class MovieResource extends JsonResource
         
     }
 
-    protected function formattedArray($propertyName)
-    {
-        $propertyValue = $this->$propertyName;
-        return $propertyValue !== null
-            ? array_map(function ($item) {
-                return ['name' => $item['name'], 'slug' => $item['slug']];
-            }, json_decode($propertyValue, true))
-            : null;
-    }
-
     protected function formattedJsonWithConfig($jsonData, $arrayConfig)
-{
-    $propertyValue = collect(json_decode($jsonData, true))->pluck('slug')->toArray();
-    $filteredArrayConfig = array_intersect_key($arrayConfig, array_flip($propertyValue));
-    $formattedJsonData = array_map(function ($name) {
-        return ['name' => $name];
-    }, $filteredArrayConfig);
+    {
+        $propertyValue = collect(json_decode($jsonData, true))->pluck('slug')->toArray();
+        $filteredArrayConfig = array_intersect_key($arrayConfig, array_flip($propertyValue));
+        $formattedJsonData = array_map(function ($name) {
+            return ['name' => $name];
+        }, $filteredArrayConfig);
 
-    return array_values($formattedJsonData);
-}
+        return array_values($formattedJsonData);
+    }
 
 }
 
