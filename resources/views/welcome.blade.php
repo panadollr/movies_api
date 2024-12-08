8a0e7fdaec20c16a645beec78f0bfc9cc466f53c
Nguyễn Văn Lân <90605381+panadollr@users.noreply.github.com>
2024-12-08 19:00:30 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -671,7 +671,7 @@
 </div>
 
 
-<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="js/jquery.js"></script>
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="{{asset('jquery.js'}}"></script>
 <!--[if lt IE 10]> <script type="text/javascript" src="js/ie8.js"></script> <![endif]-->
 <script src="{{asset('plugins.js') }}"></script>
 <script src="{{asset('contact.form.js') }}"></script>
